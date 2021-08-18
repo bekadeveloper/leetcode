738698b43acd01242ce83cfc798ca896b26b0e21
Begzod <bekadeveloper@icloud.com>
2021-08-18 20:23:14 +0500

    generate a string - one liner

--- strings/generateString.swift
+++ strings/generateString.swift
@@ -7,3 +7,10 @@ class Solution {
         }
     }
 }
+
+// one-liner 
+class Solution {
+    func generateTheString(_ n: Int) -> String {
+        return n % 2 != 0 ? String(repeating: "a", count: n) : String(repeating: "a", count: n-1) + "b"
+    }
+}
